74c834eae42eb1677ec734325eca3a58ccf1368f
Amit Patil <117425578+Asteriskkkk@users.noreply.github.com>
2025-10-16 23:22:25 +0530

    -
    fixed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["apps.mynexthire.io","gppune.ac.in"],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import { ModeToggle } from "@/components/mode-toggle";
 import { Navbar } from "@/components/navbar";
 import { useState } from "react";
 import { DATA } from "@/data/resume";
-import { GridPattern } from "@/components/ui/grid-pattern";
-import { cn } from "@/libs/utils";
+import Link from "next/link";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState("work");
@@ -20,7 +18,9 @@ export default function Home() {
             <section className="flex flex-col items-start gap-8 md:flex-row-reverse md:items-center md:justify-between">
               {/* Profile Images */}
               <div className="relative grid h-[233px] w-[175px] place-items-center md:mr-8">
-                <img
+                <Image
+                  width={175}
+                  height={233}
                   src={DATA.personal.profileImages.main}
                   alt="Ted"
                   className="absolute h-[233px] w-[175px] origin-bottom rounded-lg bg-white object-cover hover:cursor-grab active:cursor-grabbing"
@@ -61,7 +61,7 @@ export default function Home() {
 
                 {/* Action Buttons */}
                 <section className="mt-6 flex flex-wrap items-center gap-4">
-                  <a target="_blank" href={DATA.resume.url}>
+                  <Link target="_blank" href={DATA.resume.url}>
                     <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background shadow-sm hover:text-accent-foreground h-9 px-4 py-2">
                       <span className="font-semibold">{DATA.resume.downloadText}</span>
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-down ml-2 size-5">
@@ -71,11 +71,11 @@ export default function Home() {
                         <path d="m9 15 3 3 3-3"></path>
                       </svg>
                     </button>
-                  </a>
+                  </Link>
 
                   <section className="flex gap-6">
                     {DATA.socialLinks.map((link) => (
-                      <a key={link.name} href={link.url} target="_blank" className="text-muted-foreground hover:text-foreground" rel="noopener noreferrer" title={link.name}>
+                      <Link key={link.name} href={link.url} target="_blank" className="text-muted-foreground hover:text-foreground" rel="noopener noreferrer" title={link.name}>
                         <span className="sr-only">{link.name}</span>
                         {link.icon === "linkedin" && (
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin size-5" aria-hidden="true">
@@ -96,7 +96,7 @@ export default function Home() {
                             <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
                           </svg>
                         )}
-                      </a>
+                      </Link>
                     ))}
                   </section>
                 </section>
@@ -133,11 +133,11 @@ export default function Home() {
                     <ul className="ml-10 border-l">
                       {DATA.workExperiences.map((work) => (
                         <li key={work.id} className="relative ml-10 py-4">
-                          <a target="_blank" className="absolute -left-16 top-4 flex items-center justify-center rounded-full bg-white" href={work.website}>
+                          <Link target="_blank" className="absolute -left-16 top-4 flex items-center justify-center rounded-full bg-white" href={work.website}>
                             <span className="relative flex shrink-0 overflow-hidden rounded-full size-12 border">
-                              <img className="aspect-square h-full w-full bg-background object-contain" alt={work.company} src={work.logo} />
+                              <Image className="aspect-square h-full w-full bg-background object-contain" alt={work.company} src={work.logo} fill/>
                             </span>
-                          </a>
+                          </Link>
                           <div className="flex flex-1 flex-col justify-start gap-1">
                             <time className="text-xs text-muted-foreground">
                               <span>{work.startDate}</span><span> - </span><span>{work.endDate}</span>
@@ -153,7 +153,7 @@ export default function Home() {
                           {work.projects && work.projects.length > 0 && (
                             <div className="mt-2 flex flex-row flex-wrap items-start gap-2">
                               {work.projects.map((project) => (
-                                <a key={project.name} href={project.url}>
+                                <Link key={project.name} href={project.url}>
                                   <div className="items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80 flex gap-2" title={project.name}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-globe size-3" aria-hidden="true">
                                       <circle cx="12" cy="12" r="10"></circle>
@@ -162,7 +162,7 @@ export default function Home() {
                                     </svg>
                                     {project.name}
                                   </div>
-                                </a>
+                                </Link>
                               ))}
                             </div>
                           )}
@@ -179,11 +179,11 @@ export default function Home() {
                     <ul className="ml-10 border-l">
                       {DATA.education.map((edu) => (
                         <li key={edu.id} className="relative ml-10 py-4">
-                          <a target="_blank" className="absolute -left-16 top-4 flex items-center justify-center rounded-full bg-white" href={edu.website}>
+                          <Link target="_blank" className="absolute -left-16 top-4 flex items-center justify-center rounded-full bg-white" href={edu.website}>
                             <span className="relative flex shrink-0 overflow-hidden rounded-full size-12 border">
-                              <img className="aspect-square h-full w-full bg-background object-contain" alt={edu.institution} src={edu.logo} />
+                              <Image className="aspect-square h-full w-full bg-background object-contain" alt={edu.institution} src={edu.logo} fill/>
                             </span>
-                          </a>
+                          </Link>
                           <div className="flex flex-1 flex-col justify-start gap-1">
                             <time className="text-xs text-muted-foreground">
                               <span>{edu.startDate}</span><span> - </span><span>{edu.endDate}</span>
@@ -201,7 +201,7 @@ export default function Home() {
                           {edu.projects && edu.projects.length > 0 && (
                             <div className="mt-2 flex flex-row flex-wrap items-start gap-2">
                               {edu.projects.map((project) => (
-                                <a key={project.name} href={project.url}
+                                <Link key={project.name} href={project.url}
                                   target="_blank"
                                   rel="noopener noreferrer"
                                 >
@@ -213,7 +213,7 @@ export default function Home() {
                                     </svg>
                                     {project.name}
                                   </div>
-                                </a>
+                                </Link>
                               ))}
                             </div>
                           )}
@@ -229,25 +229,25 @@ export default function Home() {
             <section className="flex flex-col gap-8">
               <div className="flex justify-between">
                 <h2 className="title text-2xl sm:text-3xl">featured projects</h2>
-                <a className="link flex items-center gap-2 font-light" href="/projects">
+                <Link className="link flex items-center gap-2 font-light" href="/projects">
                   <span>view more</span>
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right size-5">
                     <path d="M5 12h14"></path>
                     <path d="m12 5 7 7-7 7"></path>
                   </svg>
-                </a>
+                </Link>
               </div>
 
               <section className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 {DATA.featuredProjects.slice(0, 2).map((project) => (
                   <div key={project.id} className="rounded-xl border bg-card text-card-foreground shadow flex flex-col">
                     <div className="flex flex-col space-y-1.5 relative p-6">
-                      <a href={project.image}>
-                        <img 
+                      <Link href={project.image}>
+                        <Image 
                           alt={project.name} 
                           loading="lazy" width="500" height="300" decoding="async" 
                           className="h-40 w-full object-cover object-top bg-white" src={project.image} />
-                      </a>
+                      </Link>
                     </div>
                     <div className="p-6 pt-0 flex flex-col gap-2">
                       <h3 className="font-semibold leading-none tracking-tight">{project.name}</h3>
@@ -265,7 +265,7 @@ export default function Home() {
                       </div>
                       <div className="flex flex-row flex-wrap items-start gap-1">
                         {project.links.website && (
-                          <a target="_blank" href={project.links.website}>
+                          <Link target="_blank" href={project.links.website}>
                             <div className="items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80 flex gap-2 px-2 py-1 text-[10px]">
                               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-globe size-3">
                                 <circle cx="12" cy="12" r="10"></circle>
@@ -274,10 +274,10 @@ export default function Home() {
                               </svg>
                               Website
                             </div>
-                          </a>
+                          </Link>
                         )}
                         {project.links.github && (
-                          <a target="_blank" href={project.links.github}>
+                          <Link target="_blank" href={project.links.github}>
                             <div className="items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80 flex gap-2 px-2 py-1 text-[10px]">
                               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-github size-3">
                                 <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
@@ -285,10 +285,10 @@ export default function Home() {
                               </svg>
                               Source
                             </div>
-                          </a>
+                          </Link>
                         )}
                         {project.links.githubUI && (
-                          <a target="_blank" href={project.links.githubUI}>
+                          <Link target="_blank" href={project.links.githubUI}>
                             <div className="items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80 flex gap-2 px-2 py-1 text-[10px]">
                               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-github size-3">
                                 <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
@@ -296,7 +296,7 @@ export default function Home() {
                               </svg>
                               Source (UI)
                             </div>
-                          </a>
+                          </Link>
                         )}
                       </div>
                     </div>
@@ -376,7 +376,7 @@ export default function Home() {
         <div className="mx-auto flex max-w-3xl flex-col items-center justify-center px-8 pb-32 sm:flex-row-reverse sm:justify-between">
           <section className="flex gap-6">
             {DATA.socialLinks.map((link) => (
-              <a key={link.name} href={link.url} target="_blank" className="text-muted-foreground hover:text-foreground" rel="noopener noreferrer" title={link.name}>
+              <Link key={link.name} href={link.url} target="_blank" className="text-muted-foreground hover:text-foreground" rel="noopener noreferrer" title={link.name}>
                 <span className="sr-only">{link.name}</span>
                 {link.icon === "linkedin" && (
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin size-5" aria-hidden="true">
@@ -397,11 +397,11 @@ export default function Home() {
                     <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
                   </svg>
                 )}
-              </a>
+              </Link>
             ))}
           </section>
           <section className="mt-8 text-center sm:mt-0 sm:text-left">
-            <p className="text-xs text-muted-foreground">© 2025 <a className="link" href="/">{DATA.personal.fullName.toLowerCase()}.com</a></p>
+            <p className="text-xs text-muted-foreground">© 2025 <Link className="link" href="/">{DATA.personal.fullName.toLowerCase()}.com</Link></p>
           </section>
         </div>
       </footer>
