fefb533bf71f72721f5d33852eb951a433a324ad
Amit Patil <117425578+Asteriskkkk@users.noreply.github.com>
2025-10-20 22:09:29 +0530

    microsoft clarity
    added microsoft clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/libs/utils";
 import { GridPattern } from "@/components/ui/grid-pattern";
+import Script from "next/script"; 
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +25,23 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tt7tb2kz37");
+            `,
+          }}
+        />
+      </head>
+
       <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
